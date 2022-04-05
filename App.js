027f8d5ad859ce0baf49430082a78cf470f463d0
Alphi Henry <aahenry51@gmail.com>
2022-04-05 18:00:36 -0400

    ThemeContext

--- App.js
+++ App.js
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, {createContext, Button} from 'react';
 
-const App: () => Node = () => {};
+const ThemeContext = createContext();
+
+const ThemedButton = () => (
+  <ThemeContext.Consumer>
+    {value => (
+      <Button primaryColor={{primaryColor: 'green'}}>
+        I'm button using context!
+      </Button>
+    )}
+  </ThemeContext.Consumer>
+);
+
+const App = props => (
+  <ThemeContext.Provider value={{primaryColor: 'green'}}>
+    {props.children}
+  </ThemeContext.Provider>
+);
 
 export default App;
